a76b9743ef4e5a9fd793a1a8233fe2fc1081c8fb
Porto1090 <A01027893@tec.mx>
2025-04-24 00:27:01 -0600

    feat: enable react components in astro via @astrojs/react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss(), basicSsl()],
     server: {
