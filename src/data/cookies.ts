2cbb61d6ce6873f4e0f697af76f17d43e85adc7f
Eduardo Porto Morales <165714406+Porto1090@users.noreply.github.com>
2025-05-21 12:38:34 -0700

    fix: restore fake cookies content

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -4,7 +4,7 @@ const mockCookies = {
     username: "John Doe",
     id: "3",
     department_id: "3",
-    role: "AccountsPayable" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    role: "Applicant" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
