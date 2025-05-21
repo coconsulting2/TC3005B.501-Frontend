c57fa36a1ea028ef9d4c1c44bbb88d825422dd19
paydelimon22 <valentina2212.gonzalez@gmail.com>
2025-05-20 21:06:26 -0600

    refactor: modified cookies to include a users department_id

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -2,8 +2,9 @@ import type { UserRole } from "@type/roles";
 
 const mockCookies = {
     username: "John Doe",
-    id: "1",
-    role: "Applicant" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    id: "3",
+    department_id: "3",
+    role: "AccountsPayable" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
