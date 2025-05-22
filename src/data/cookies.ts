38aa56a944cb40da2de2b96efa4787a136c22b82
Gwenvito <toootiz.killer@gmail.com>
2025-05-21 19:26:25 -0600

    fix: some changes

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -4,7 +4,7 @@ const mockCookies = {
     username: "John Doe",
     id: "2",
     department_id: "1",
-    role: "TravelAgency" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    role: "Authorizer" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
