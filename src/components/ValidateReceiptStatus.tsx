2fe5119af7ce0b9ab51bfd1b380807d4ef994b77
José <A01028517@tec.mx>
2025-05-28 14:33:07 -0600

    feat: connection with backend to send receipts to validation

---
+++ src/components/ValidateReceiptStatus.tsx
@@ -0,0 +1,52 @@
+import { useCallback } from "react";
+import { apiRequest } from "@utils/apiClient";
+import ModalWrapper from "@components/ModalWrapper";
+
+interface Props {
+  request_id: number;
+  title: string;
+  message: string;
+  redirection: string;
+  modal_type: "success" | "warning";
+  variant?: "primary" | "secondary";
+  children: React.ReactNode;
+}
+
+export default function ValidateReceiptStatus({
+  request_id,
+  title,
+  message,
+  redirection,
+  modal_type,
+  variant,
+  children,
+}: Props) {
+  const handleConfirm = useCallback(async () => {
+    try {
+      const url = `/applicant/send-expense-validation/${request_id}`;
+      await apiRequest(url, { method: "PUT" });
+      alert(`Comprobante enviado exitosamente.`)
+
+      if (redirection) {
+        window.location.href = redirection;
+      } else {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+    }
+  }, [request_id, redirection]);
+
+  return (
+    <ModalWrapper
+      title={title}
+      message={message}
+      button_type={modal_type}
+      modal_type={modal_type}
+      onConfirm={handleConfirm}
+      variant={variant}
+    >
+      {children}
+    </ModalWrapper>
+  );
+}
