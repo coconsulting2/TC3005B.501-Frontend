1b1777cbae70313e8d674cc664c558b61e6cb95c
JoseGlezMtz <A01028517@tec.mx>
2025-05-28 20:31:34 -0600

    fech:

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -4,7 +4,7 @@ const mockCookies = {
     username: "John Doe",
     id: "4",
     department_id: "1",
-    role: "TravelAgency" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    role: "Applicant" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
