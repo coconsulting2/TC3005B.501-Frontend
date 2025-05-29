50b6118508df3f82edafc4f574e8bf327217d0d6
JoseGlezMtz <A01028517@tec.mx>
2025-05-28 20:15:05 -0600

    chore: implement aprove Request in TravelAgency and correction

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -2,9 +2,9 @@ import type { UserRole } from "@type/roles";
 
 const mockCookies = {
     username: "John Doe",
-    id: "2",
+    id: "4",
     department_id: "1",
-    role: "Applicant" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    role: "TravelAgency" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
