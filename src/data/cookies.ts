408686150457c2ddeacafc8427f137b9f1a7b959
SnakeJazzzz <michael2506@icloud.com>
2025-05-31 13:25:00 -0600

    WIP: admin users page

--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -2,7 +2,7 @@ import type { UserRole } from "@type/roles";
 
 const mockCookies = {
     username: "John Doe",
-    role: "Authorizer" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
+    role: "Admin" as UserRole //'Applicant' | 'Authorizer' | 'Admin' | 'AccountsPayable' | 'TravelAgency';
 };
 
 export const getCookie = (key: keyof typeof mockCookies): string | UserRole => {
