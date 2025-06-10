1f6e7fdc7783e125d8953a00890e940beed13a88
Porto1090 <A01027893@tec.mx>
2025-06-09 19:54:14 -0600

    feat: add ultimatewrapper for upcoming changes

---
+++ src/components/UltimateWrapper.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useState } from "react";
+import { apiRequest } from "@utils/apiClient";
+import ModalWrapper from "@components/ModalWrapper";
+import Toast from "@components/Toast";
+
+interface Props {
+	user_id: number;
+  endpoint: string;
+  title: string;
+  message: string;
+  modal_type: "success" | "warning";
+  children: React.ReactNode;
+  token: string;
+}
+
+export default function UltimateWrapper({ 
+	user_id,
+	endpoint,
+	title,
+	message,
+	modal_type,
+	children,
+	token,
+}: Props) {
+	const [toast, setToast] = useState<{ message: string; type: 'success' | 'error' } | null>(null);
+	const handleConfirm = useCallback(async () => {
+		try {
+			const url = `${endpoint}/${user_id}`;
+			await apiRequest(url, { 
+				method: "PUT",
+				headers: { Authorization: `Bearer ${token}` }
+			});
+			setToast({ message: 'Usuario desactivado exitosamente.', type: 'success' });
+			await new Promise(resolve => setTimeout(resolve, 2000));
+		} catch (error) {
+			console.error("Error en la solicitud:", error);
+		}
+	}, [endpoint]);
+
+	return (
+		<>
+			<ModalWrapper
+				title={title}
+				message={message}
+				button_type={modal_type}
+				modal_type={modal_type}
+				onConfirm={handleConfirm}
+			>
+				{children}
+			</ModalWrapper>
+			{toast && <Toast message={toast.message} type={toast.type} />}
+		</>
+	);
+}
