c1a704a1f0d5d3615a17c7f78371e0a75851fd83
JoseGlezMtz <A01028517@tec.mx>
2025-06-10 22:26:48 -0600

    feat: modification of the test for correct functionality

--- cypress/e2e/document-validation.cy.js
+++ cypress/e2e/document-validation.cy.js
@@ -1,26 +1,68 @@
+import 'cypress-file-upload';
 describe('Document Validation', () => {
     beforeEach(() => {
       // Navigate to the page where the file upload is located
-      cy.visit('/comprobar-gastos/1'); // Replace with the actual route
+      cy.visit('https://localhost:4321');
+
+    cy.get('input[placeholder="Usuario"]').type('Solicitante101');
+    cy.get('input[placeholder="Contraseña"').type('123{enter}');
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('Inicio de sesión exitoso');
+    });
+    cy.on('window.confirm', ()=> true);
+
+    cy.url().should('include', 'dashboard'); 
+
+    cy.get('a[href="/comprobar-gastos"]').click();
+
+    cy.url().should('include', 'comprobar-gastos');
+
+    cy.get('a[href^="/comprobar-solicitud/"]').click();
+    cy.get('a[href^="/subir-comprobante/"]').click();  
+
     });
   
     it('should accept valid XML and PDF files', () => {
-      // Upload a valid PDF file
-      cy.get('input[type="file"]').attachFile('valid-document.pdf');
-      cy.contains('Archivo cargado correctamente').should('exist');
+
+      
+      cy.get('select[name="concepto"]').select('Hospedaje');
+      cy.get('input[type="number"]').type('123.45');
+      // Subir archivo PDF
+      cy.get('input[type="file"][accept=".pdf"]')
+      .attachFile('valid-document.pdf');
   
       // Upload a valid XML file
-      cy.get('input[type="file"]').attachFile('valid-document.xml');
-      cy.contains('Archivo cargado correctamente').should('exist');
+      cy.get('input[type="file"][accept=".xml"]')
+      .attachFile('valid-document.xml');
+
+      cy.contains('button', 'Subir Comprobante').click();
+      cy.contains('button', 'Confirmar').click();
+      cy.on('window:alert', (text) => {
+        expect(text).to.contains('Subidos correctamente');
+      });
+      cy.on('window.confirm', ()=> true);
+      
+
+
+
+
     });
   
     it('should reject invalid file formats', () => {
+
+      cy.get('select[name="concepto"]').select('Hospedaje');
+      cy.get('input[type="number"]').type('123.45');
       // Upload an invalid file (e.g., .jpg)
       cy.get('input[type="file"]').attachFile('invalid-document.jpg');
-      cy.contains('Formato de archivo no permitido').should('exist');
   
       // Upload another invalid file (e.g., .txt)
       cy.get('input[type="file"]').attachFile('invalid-document.txt');
-      cy.contains('Formato de archivo no permitido').should('exist');
+
+      cy.contains('button', 'Subir Comprobante').click();
+      cy.contains('button', 'Confirmar').click();
+      cy.on('window:alert', (text) => {
+        expect(text).to.contains('Por favor, completa todos los campos correctamente.');
+      });
     });
   });
